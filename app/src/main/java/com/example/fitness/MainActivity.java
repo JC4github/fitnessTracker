58f6849dcca25c39506a401cce308ff7d914ba14
JACE <jacexin77@gmail.com>
2022-04-30 21:57:20 +1200

    got button to work

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button back;
+    private ImageButton bicep;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +26,35 @@ public class MainActivity extends AppCompatActivity {
                 openBackActivity();
             }
         });
+
+        bicep = findViewById(R.id.bicepBtn);
+        bicep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeVisible(bicep);
+            }
+        });
     }
     public void openBackActivity(){
         Intent intent = new Intent(this, MainActivity2_backpage.class);
         startActivity(intent);
+    }
+
+    private void changeVisible(View view) {
+        if(view.getVisibility()==View.VISIBLE)
+        {
+            view.setVisibility(View.INVISIBLE);
+            return;
+        }
+
+        view.setVisibility(View.VISIBLE);
     }
 }
