025874bd1914959e2105b36524bb63d5caa435b1
JACE <jacexin77@gmail.com>
2022-05-01 11:26:23 +1200

    finished setting up front activity buttons

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -7,13 +7,73 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button back;
-    private ImageButton bicep;
-    private Button button;
+
+    //all images
+    private ImageView bicep;
+    private ImageView abs;
+    private ImageView traps;
+    private ImageView chest;
+    private ImageView shoulder;
+    private ImageView quad;
+    private ImageView calf;
+    private ImageView forearm;
+
+    //all buttons
+    private Button bicepBtnL;
+    private Button bicepBtnR;
+    private Button absBtn;
+    private Button forearmBtnL;
+    private Button forearmBtnR;
+    private Button trapsBtn;
+    private Button chestBtn;
+    private Button shoulderBtnL;
+    private Button shoulderBtnR;
+    private Button quadBtnL;
+    private Button quadBtnR;
+    private Button calfBtnL;
+    private Button calfBtnR;
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bicepBtnL:
+            case R.id.bicepBtnR:
+                changeVisible(bicep);
+                break;
+            case R.id.forearmBtnL:
+            case R.id.forearmBtnR:
+                changeVisible(forearm);
+                break;
+            case R.id.shoulderBtnL:
+            case R.id.shoulderBtnR:
+                changeVisible(shoulder);
+                break;
+            case R.id.quadBtnL:
+            case R.id.quadBtnR:
+                changeVisible(quad);
+                break;
+            case R.id.calf1BtnL:
+            case R.id.calf1BtnR:
+                changeVisible(calf);
+                break;
+            case R.id.trapsBtn:
+                changeVisible(traps);
+                break;
+            case R.id.chestBtn:
+                changeVisible(chest);
+                break;
+            case R.id.absBtn:
+                changeVisible(abs);
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +87,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        bicep = findViewById(R.id.bicepBtn);
-        bicep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
-            }
-        });
+        //initialise all images
+        bicep = findViewById(R.id.bicep);
+        shoulder = findViewById(R.id.shoulder1);
+        forearm = findViewById(R.id.forearm1);
+        chest = findViewById(R.id.chest);
+        abs = findViewById(R.id.abs);
+        quad = findViewById(R.id.quad);
+        calf = findViewById(R.id.calf1);
+        traps = findViewById(R.id.traps1);
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                changeVisible(bicep);
-            }
-        });
+        //initialise all buttons
+        bicepBtnL = findViewById(R.id.bicepBtnL);
+        bicepBtnL.setOnClickListener(this);
+        bicepBtnR = findViewById(R.id.bicepBtnR);
+        bicepBtnR.setOnClickListener(this);
+
+        shoulderBtnL = findViewById(R.id.shoulderBtnL);
+        shoulderBtnL.setOnClickListener(this);
+        shoulderBtnR = findViewById(R.id.shoulderBtnR);
+        shoulderBtnR.setOnClickListener(this);
+
+        chestBtn = findViewById(R.id.chestBtn);
+        chestBtn.setOnClickListener(this);
+
+        absBtn = findViewById(R.id.absBtn);
+        absBtn.setOnClickListener(this);
+
+        quadBtnL = findViewById(R.id.quadBtnL);
+        quadBtnL.setOnClickListener(this);
+        quadBtnR = findViewById(R.id.quadBtnR);
+        quadBtnR.setOnClickListener(this);
+
+        forearmBtnL = findViewById(R.id.forearmBtnL);
+        forearmBtnL.setOnClickListener(this);
+        forearmBtnR = findViewById(R.id.forearmBtnR);
+        forearmBtnR.setOnClickListener(this);
+
+        calfBtnL = findViewById(R.id.calf1BtnL);
+        calfBtnL.setOnClickListener(this);
+        calfBtnR = findViewById(R.id.calf1BtnR);
+        calfBtnR.setOnClickListener(this);
+
+        trapsBtn = findViewById(R.id.trapsBtn);
+        trapsBtn.setOnClickListener(this);
     }
+
+
     public void openBackActivity(){
         Intent intent = new Intent(this, MainActivity2_backpage.class);
         startActivity(intent);
